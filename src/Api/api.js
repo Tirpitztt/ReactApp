e694e08d73d5c38eb25676c28cdbd5537f71f161
Tirpitztt <48515750+Tirpitztt@users.noreply.github.com>
2021-03-21 18:21:58 +0300

    refactor DAL api

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,8 +1,25 @@
 import axios from "axios"
 
-export const getUsers = (currentPage=1,pageSize=20)=>{
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-    {withCredentials:true})
-    .then(response => response.data);
+const instanceAx = axios.create({
+    baseURL:'https://social-network.samuraijs.com/api/1.0/',
+    headers:{'API-KEY': 'b9327212-b8d3-4fc5-a951-7732f4aa6fdd'},
+    withCredentials:true
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 20) {
+        return instanceAx.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    },
+
+    userFollow(userId){
+        return instanceAx.post(`follow/${userId}`)
+            .then(response => response.data)
+    },
+    userUnfollow(userId){
+        return instanceAx.delete(`follow/${userId}`)
+            .then(response => response.data)
+    }
+
 }
 
--- src/components/Users/User/UsersClas.jsx
+++ src/components/Users/User/UsersClas.jsx
@@ -2,15 +2,14 @@ import * as React from "react";
 import Users from "../Users";
 import axios from "axios";
 import Preloader from "../../Common/Preloader/Preloader";
-import { getUsers } from "../../../Api/api";
+import { usersAPI} from "../../../Api/api";
 
 
 class UsersClas extends React.Component{
 
      componentDidMount() {
          this.props.setFetching(true);
-        
-            getUsers(this.props.state.users.currentPage,this.props.state.users.pageSize)
+         usersAPI.getUsers(this.props.state.users.currentPage,this.props.state.users.pageSize)
             .then(data=>{
                 this.props.setFetching(false);
                 this.props.setTotalCount(data.totalCount);
@@ -21,8 +20,7 @@ class UsersClas extends React.Component{
     pageChange = (page)=>{
         this.props.setCurrentPage(page);
         this.props.setFetching(true);
-        
-            getUsers(page,this.props.state.users.pageSize)
+        usersAPI.getUsers(page,this.props.state.users.pageSize)
             .then(data=>{
                 this.props.setFetching(false);
                 this.props.setTotalCount(data.totalCount);
@@ -33,35 +31,22 @@ class UsersClas extends React.Component{
     followChange = (userId,flag)=>{
         if(!flag){
             this.props.setFetching(true);
-            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`,null,{
-                withCredentials:true,
-                headers:{
-                    'API-KEY': 'b9327212-b8d3-4fc5-a951-7732f4aa6fdd'
-                }
-            }).then(response=>{
+            usersAPI.userFollow(userId).then(data=>{
                 this.props.setFetching(false);
-                if(response.data.resultCode===0){
+                if(data.resultCode===0){
                     this.props.setUserFollow(userId);
                 }
-                
             })
         }else {
             this.props.setFetching(true);
-            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`,{
-                withCredentials:true,
-                headers:{
-                    'API-KEY': 'b9327212-b8d3-4fc5-a951-7732f4aa6fdd'
-                }
-            }).then(response=>{
+            usersAPI.userUnfollow(userId).then(data=>{
                 this.props.setFetching(false);
-                if(response.data.resultCode===0){
+                if(data.resultCode===0){
                     this.props.setUserFollow(userId);
                 }
-                
-            })
+             })
         }
-        
-    }
+     }
     render(){
         return(
             <>
